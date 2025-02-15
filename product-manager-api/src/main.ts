39eee165f4e60d6a24a04afdcba3aafb638e8fc5
lebaliro <lebaliro10@gmail.com>
2025-02-15 05:25:15 -0300

    feat: added validation global pipe

--- product-manager-api/src/main.ts
+++ product-manager-api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerGlobal } from './common/logger/logger.provider';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(LoggerGlobal));
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
