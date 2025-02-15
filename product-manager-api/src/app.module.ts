746a04c54f22c3349546d6ab909c72c4fac42a7f
lebaliro <lebaliro10@gmail.com>
2025-02-15 05:24:41 -0300

    feat: added prisma service

--- product-manager-api/src/app.module.ts
+++ product-manager-api/src/app.module.ts
@@ -4,15 +4,15 @@ import { IaModule } from './ia/ia.module';
 import { APP_FILTER } from '@nestjs/core';
 import { CatchFilter } from './common/filters/exception.filter';
 import { LoggerModule } from './common/logger/logger.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProductsModule, IaModule, LoggerModule],
+  imports: [ProductsModule, IaModule, LoggerModule, PrismaModule],
   providers: [
     {
       provide: APP_FILTER,
       useClass: CatchFilter,
     },
-
   ],
 })
 export class AppModule {}
